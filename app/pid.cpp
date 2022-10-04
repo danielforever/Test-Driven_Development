05ce2752a71c205a111a487e3add2c06853cd9c4
silver <15abhinavgarg@gmail.com>
2022-10-04 18:21:49 -0400

    Completing compute function

--- app/pid.cpp
+++ app/pid.cpp
@@ -1,39 +1,46 @@
 /**
  * @file pid.cpp
- * @author 15abhinavgarg longhongc
- * @brief 
+ * @author Driver: 15abhinavgarg Navigator: longhongc
+ * @brief
  * @version Part 2 implementation of TTD
  * @date 2022-10-04
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
-
+#include <bits/stdc++.h>  // importing stl
 #include <pid.hpp>
-#include <bits/stdc++.h>
 
-
-double PID::compute(double SetPoint,double Va) {  
-  
+double PID::compute(double SetPoint, double Va) {
   // calculating the error
   double error = SetPoint - Va;
 
   // pushing error to vector list;
-  Errors.push_back(error);
+  // Errors.push_back(error);
   // check the vector function
-  double Pout = kp * error;
+  double Pout = Kp * error;
 
+  double Iout;
+  if (Errors.size() == 0) {
+    Iout = 0;
+  } else {
+    double sum = accumulate(Errors.begin(), Errors.end(), 0);
+    Iout = Ki * sum * dt;
+  }
 
-  double sum = accumulate(Errors.begin(), Errors.end(),0);
-  double Iout = Ki * sum * dt;
+  double Dout;
+  if (Errors.size() == 0) {
+    Dout = 0;
+  } else {
+    double perror = Errors[Errors.size() - 1];
+    Dout = Kd * (error - perror) / (dt);
+  }
 
-  
-  double perror = Errors[ Errors.size() - 2];
-  double Dout = Kd * (error - perror)/(dt);
-
+  // double perror = Errors[ Errors.size() - 1];
+  // double Dout = Kd * (error - perror)/(dt);
+  Errors.push_back(error);
   double Output = Pout + Iout + Dout;
-   
-  return Output; 
-}
 
+  return Output;
+}
