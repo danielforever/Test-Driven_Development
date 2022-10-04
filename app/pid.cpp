430dbefd89ce6019e8f88be1f321df99944733b1
silver <15abhinavgarg@gmail.com>
2022-10-04 15:10:00 -0400

    adding compute functionality

--- app/pid.cpp
+++ app/pid.cpp
@@ -1,5 +1,33 @@
+/**
+ * @file pid.cpp
+ * @author 15abhinavgarg longhongc
+ * @brief 
+ * @version Part 2 implementation of TTD
+ * @date 2022-10-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
 #include <pid.hpp>
+#include <bits/stdc++.h>
+
 
 double PID::compute(double SetPoint,double Va) {  
+  
+  // calculating the error
+  double error = SetPoint - Va;
+
+  Errors.push_back(error*dt);
+
+  double Pout = kp * error;
+  double sum = accumulate(Errors.begin(), Errors.end(),0);
+  
+  double 
+
+  return PID->compute(SetPoint,Va)
+  
+  
   return 0.0; 
 }
