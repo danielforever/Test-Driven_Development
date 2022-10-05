9aa3b1b4c5cb7ffcec1a051d52523fa73c47eb1d
danielforever <daniel87011354@gmail.com>
2022-10-05 23:44:51 +0800

    Change the test case pid3 to a testable one.

--- test/test.cpp
+++ test/test.cpp
@@ -12,11 +12,10 @@ TEST(unit_compute, case2) {
   EXPECT_DOUBLE_EQ(pid2.compute(1.0,2.0), -0.1);
 }
 
-TEST(unit_compute, case3) {
-		
+TEST(unit_compute, case3) {	
   EXPECT_DOUBLE_EQ(pid3.compute(1.0,2.0), -0.1);
   EXPECT_DOUBLE_EQ(pid3.compute(1.0,-0.1), 21.109999);
-  EXPECT_DOUBLE_EQ(pid3.compute(1.0,21.109999), -214.1109898);
+  EXPECT_NEAR(pid3.compute(1.0,21.109999), -214.1109898, 1e-5);
 }
 
 
